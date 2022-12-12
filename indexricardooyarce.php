e364a8114e6e797333833f16a06c30cb8f766ebe
Kuzunoha <44280935+kuzunoharem@users.noreply.github.com>
2022-12-12 16:53:53 -0300

    Update indexricardooyarce.php

--- indexricardooyarce.php
+++ indexricardooyarce.php
@@ -11,11 +11,11 @@
 <?php
 class MyClass {
 	public static $str = "<h3>Hi my name is Richard Oyarce";
-	public static $JR = "Administracion de Servicios Web <br>";
+	public static $RO = "Administracion de Servicios Web <br>";
 	
 }
 echo MyClass::$str."<br>";
-echo MyClass::$JR; "<br>";
+echo MyClass::$RO; "<br>";
 echo "La fecha actual es " .date("d/m/y") . "<br>";
 date_default_timezone_set("America/Santiago");
 echo "La hora es: " .date("G:i:s");
